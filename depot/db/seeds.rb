9315eefd16fe7c4859e2b93dcee8467d5bf5f91e
Jacob <pttr.jcb@gmail.com>
2014-10-07 16:11:01 -0400

    Test commit

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -16,34 +16,8 @@ you should add Ruby to your toolbox.
 </p>},
 image_url: 'ruby.jpg',
 price: 49.95)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-description:
-%{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-image_url: 'ruby.jpg',
-price: 49.95)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-description:
-%{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-image_url: 'ruby.jpg',
-price: 49.95)
-Product.create!(title: 'Fuck off',
-description:
-%{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-image_url: 'ruby.jpg',
-price: 49.95)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+
+Product.create!(title: 'Notes from underground',
 description:
 %{<p>
 Ruby is the fastest growing and most exciting dynamic language
@@ -51,24 +25,4 @@ out there. If you need to get working programs delivered fast,
 you should add Ruby to your toolbox.
 </p>},
 image_url: 'cs.jpg',
-price: 49.95)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-description:
-%{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-image_url: 'ruby.jpg',
-price: 49.95)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-description:
-%{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your toolbox.
-</p>},
-image_url: 'ruby.jpg',
-price: 49.95)
-
-# . . .
+price: 40.0)
